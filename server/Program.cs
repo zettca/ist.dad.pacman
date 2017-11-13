68b97e7399e880aa6a2b0854613149e2f1e7b70b
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2017-11-13 16:11:48 +0000

    Add server project to solution

---
+++ server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("╔════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                    Pacman Server                   ║");
+            Console.WriteLine("║             DAD 2017-2018, IST - Group 4           ║");
+            Console.WriteLine("╚════════════════════════════════════════════════════╝");
+
+            Console.ReadLine();
+        }
+    }
+}
